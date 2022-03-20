acb14d0047998c065bde52a8219c1083dd8928fc
valov-junior <65654891+valov-junior@users.noreply.github.com>
2022-03-20 14:47:04 +0200

    newsite

---
+++ custom.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function(){
+    $('.comments_slider').bxSlider({
+        mode:'horizontal',
+        speed: 100,
+        pager: true,
+        controls: true,
+        minSlides: 1,
+        maxSlides: 3,
+        slideWidth:400,
+        auto: false,
+    });
+});
